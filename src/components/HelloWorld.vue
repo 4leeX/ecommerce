f632b5d2dea6c35f4c8c11a334cce15d7487de33
Alexsandro <alexsandro.sousa12366@gmail.com>
2022-09-14 19:58:52 -0300

    add navbar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="homeContainer">
 
+    <MenuCategorias />
+
+  <div class="cardContainerProvisory">
     <div v-for="(p, i) in products" :key="i"
       class="cardContainer">
       <div class="cardImg">
@@ -11,16 +14,21 @@
       </div>
     </div>
   </div>
+  </div>
 </template>
 
 <script>
 import products from '../service/products';
+import MenuCategorias from '../components/MenuCategorias.vue';
 
 export default {
   data(){
     return{
       products: [],
     }
+  },
+  components:{
+    MenuCategorias
   },
   mounted(){
     this.getProducts();
@@ -40,39 +48,48 @@ export default {
 <style lang="scss" scoped>
 .homeContainer{
   display: flex;
+  flex-direction: column;
   flex-wrap: wrap;
   padding: 30px;
   justify-content: center;
+    
+  .cardContainerProvisory{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding: 30px;
+    /* flex-direction: column; */
 
-  .cardContainer{
-    width: 300px;
-    height: 400px;
-    padding: 0 5px;
-
-    .cardImg{
-      padding: 10px;
-      height: 50%;
-      background: #FFF;
-      border-radius: 10px 10px 0 0px;
-
-      img{
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        min-width: 100px;
-        height: 100%;
+    .cardContainer{
+      width: 300px;
+      height: 400px;
+      padding: 0 5px;
+  
+      .cardImg{
+        padding: 10px;
+        height: 50%;
+        background: #FFF;
+        border-radius: 10px 10px 0 0px;
+  
+        img{
+          display: block;
+          margin-left: auto;
+          margin-right: auto;
+          min-width: 100px;
+          height: 100%;
+        }
       }
-    }
-    .cardContent{
-      padding: 15px 5px 5px 5px;
-      height: 150px;
-      background: grey;
-      border-radius: 0 0 10px 10px;
-      
-      p{
-        max-height: 34px;
-        overflow: hidden;
-        text-overflow: ellipsis;
+      .cardContent{
+        padding: 15px 5px 5px 5px;
+        height: 150px;
+        background: grey;
+        border-radius: 0 0 10px 10px;
+        
+        p{
+          max-height: 34px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
       }
     }
   }
---
+++ src/components/MenuCategorias.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <div class="categoriaContainer">
+        <div class="imgCategoria">
+            <img src="" alt="">a
+        </div>
+        <div class="textContent">
+            <p>Teste</p>
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .categoriaContainer{
+        width: 100%;
+        height: 100px;
+        background: red;
+    }
+</style>
