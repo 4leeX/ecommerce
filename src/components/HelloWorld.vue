331a7a058bc4ebfb29a3b4d75a737e000b353172
Alexsandro <alexsandro.sousa12366@gmail.com>
2022-09-17 16:17:03 -0300

    ajuste

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -33,7 +33,7 @@
 </template>
 
 <script>
-import products from '../service/products';
+import products from '../service/products.js';
 import MenuCategorias from '../components/MenuCategorias.vue';
 import SecaoMedia from '../components/SecaoMedia.vue';
 
