168ba21f88e6e72911aeef8bac6a05d13940d2be
Alexsandro <alexsandro.sousa12366@gmail.com>
2022-09-17 17:29:06 -0300

    e la vamos nos

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
-import store from './store/index.js';
+import store from './store/index';
 import './style.scss'
 import App from './App.vue'
+
+console.log(store);
 
 createApp(App).use(store).mount('#app')
