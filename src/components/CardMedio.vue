d65896b02fcc9c138bda8d434ac335881d828ad0
Alexsandro <alexsandro.sousa12366@gmail.com>
2022-09-17 20:02:37 -0300

    ajuste card medio

--- src/components/CardMedio.vue
+++ src/components/CardMedio.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="cardMedioContainer">
+  <div class="cardMedioContainer" 
+    :class="tipo=='img' ? 'img-card' : 'prod-card'">
     <h2>{{texto}}</h2>
-
+    <div class="img-card" :style="tipo=='card' ? 'display:none':''">
+      <img :src="pathImg" alt="imagem">
+    </div>
     <a href="#">Ver mais</a>
   </div>
 </template>
@@ -13,13 +16,22 @@ export default {
       products: [],
     }
   },
-  props: ['tipo', 'texto'],
+  props: ['tipo', 'texto', 'pathImg'],
   mounted(){},
   methods: {}
 }
 </script>
 
 <style lang="scss" scoped>
+    /* .img-card{
+      img{
+        max-height: 100%;
+        max-width: 100%;
+      }
+    }
+    .prod-card{ */
+
+    /* } */
     .cardMedioContainer{
         width: 25%;
         background: #FFF;
@@ -29,9 +41,30 @@ export default {
         text-align: center;
         box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 
+        a{
+          font-size: 15px;
+          text-decoration: none;
+          &:hover{
+            color: grey;
+            text-decoration: underline;
+            text-decoration-color: grey;
+          }
+        }
         h2{
             border-bottom: 1px solid rgb(207, 207, 207);
             padding-bottom: 10px;
+        }
+
+        .img-card{
+          width: 100%;
+          height: 300px;
+          padding: 5px 0;
+          img{
+            border-radius: 10px;
+            max-height: 100%;
+            min-width: 100%;
+
+          }
         }
     }
 </style>
